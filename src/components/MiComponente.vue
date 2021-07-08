9e85e9fcee4cbab73001ab5bc89b8285c9868590
Yuriy MP <yuriy.mraka@gmail.com>
2021-07-08 21:42:29 +0200

    feat: my first component MiComponente

---
+++ src/components/MiComponente.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>Mi primer componente con Vue</p>
+</template>
+
+<script>
+export default {
+  name: "MiComponente",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
